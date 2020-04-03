80467f4212854e7fac0bf7110003d11bfdb2fab8
okostroma <oxano4ka96@tut.by>
2020-04-03 21:29:02 +0300

    Updates

---
+++ precache-manifest.c53df3c9383fe6ea84499ba1b04429c5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "426bdaa47583b34c20d14190c8014df7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2eee1a2248f6d68ef2a2",
+    "url": "/portfolio/static/css/main.6d939ee9.chunk.css"
+  },
+  {
+    "revision": "174c65faf407840b7061",
+    "url": "/portfolio/static/js/2.9b3b6e2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.9b3b6e2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eee1a2248f6d68ef2a2",
+    "url": "/portfolio/static/js/main.432a1647.chunk.js"
+  },
+  {
+    "revision": "1e74db4740f99c68a0bf",
+    "url": "/portfolio/static/js/runtime-main.b37861ed.js"
+  },
+  {
+    "revision": "5c1c7ccc244099e5c22a136be240121b",
+    "url": "/portfolio/static/media/avatar.5c1c7ccc.jpg"
+  },
+  {
+    "revision": "1a5eb55ae0b2caf1b47408a2fe6fce31",
+    "url": "/portfolio/static/media/grid.1a5eb55a.png"
+  },
+  {
+    "revision": "f50ceefd2bce0c31f011626496023827",
+    "url": "/portfolio/static/media/logo.f50ceefd.png"
+  }
+]);
