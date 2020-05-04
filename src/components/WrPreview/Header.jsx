7d115c89065a85e1417161b71d8c386732eb16bf
Oxa <oxakost@gmail.com>
2020-05-04 22:48:52 +0300

    mobile-2

--- src/components/WrPreview/Header.jsx
+++ src/components/WrPreview/Header.jsx
@@ -2,26 +2,46 @@ import React from 'react';
 import logo from './logo.png'
 import classes from './Header.module.css'
 
-const Header = () => {
-    // let header = document.querySelector('.header');
-    //
-    // if(header) {
-    //     window.onScroll = () => {
-    //         if(document.documentElement.scrollTop > 600){
-    //             header.classList.add(".headerFixed");
-    //         }
-    //         else{
-    //             header.classList.remove(".headerFixed");
-    //         }
-    //     }
-    // }
+class Header extends React.Component {
 
-    return (
+    state = {
+        isActive: false
+    }
+
+    onClickBtn = () => {
+        this.setState({
+            isActive: true
+        })
+        if (this.state.isActive) {
+            this.setState({
+                isActive: false
+            })
+        }
+    }
+
+
+    render() {
+        let active = this.state.isActive ? classes.activeBtn : '';
+
+        return (
             <header className={classes.header}>
                 <div className={classes.container}>
                     <a href="#"><img className={classes.logo} src={logo}/></a>
+
+                    <div onClick={this.onClickBtn} className={classes.btnMenu}>
+                        <div className={`${classes.btnLines} ${active}`}>
+                            <nav className={classes.activeNav}>
+                                <ul>
+                                    <li><a className="link" href="#">Main</a></li>
+                                    <li><a className="link" href="#">Skills</a></li>
+                                    <li><a className="link" href="#">Works</a></li>
+                                    <li><a className="link" href="#">Contacts</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
                     <div className={classes.containerNav}>
-                        <nav className="menu-desktop" id="menu-desktop">
+                        <nav>
                             <ul>
                                 <li><a className="link" href="#">Main</a></li>
                                 <li><a className="link" href="#">Skills</a></li>
@@ -30,10 +50,13 @@ const Header = () => {
                             </ul>
                         </nav>
                     </div>
+
+
                 </div>
             </header>
 
-    );
+        );
+    }
 }
 
 export default Header;
