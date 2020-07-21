421c182df3250e548fa0e5240df1c21f7ab3c384
Oxa <oxakost@gmail.com>
2020-07-21 13:40:20 +0300

    Updates

---
+++ precache-manifest.76bff38daef94970d6759099d6314b34.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f0dfd1686d76cc732abb0d9b0dabe12",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b1707b5acc2f163eba9a",
+    "url": "/portfolio/static/css/main.8982097b.chunk.css"
+  },
+  {
+    "revision": "98e7bc5cdff9a46312ef",
+    "url": "/portfolio/static/js/2.49cf6b6d.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/portfolio/static/js/2.49cf6b6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1707b5acc2f163eba9a",
+    "url": "/portfolio/static/js/main.09fe22e5.chunk.js"
+  },
+  {
+    "revision": "1e74db4740f99c68a0bf",
+    "url": "/portfolio/static/js/runtime-main.b37861ed.js"
+  },
+  {
+    "revision": "5c1c7ccc244099e5c22a136be240121b",
+    "url": "/portfolio/static/media/avatar.5c1c7ccc.jpg"
+  },
+  {
+    "revision": "caed10d8b4a30d853551176cf59937e5",
+    "url": "/portfolio/static/media/avenders-game.caed10d8.png"
+  },
+  {
+    "revision": "7b9bc94cd7bdb97e41bf884a1b967454",
+    "url": "/portfolio/static/media/counter.7b9bc94c.png"
+  },
+  {
+    "revision": "1a5eb55ae0b2caf1b47408a2fe6fce31",
+    "url": "/portfolio/static/media/grid.1a5eb55a.png"
+  },
+  {
+    "revision": "f50ceefd2bce0c31f011626496023827",
+    "url": "/portfolio/static/media/logo.f50ceefd.png"
+  },
+  {
+    "revision": "79c62aa223bb655bc25889a15c4cd75f",
+    "url": "/portfolio/static/media/net.79c62aa2.png"
+  },
+  {
+    "revision": "7584dd31c6421106e3dad5290496e8fe",
+    "url": "/portfolio/static/media/todolist.7584dd31.png"
+  }
+]);
